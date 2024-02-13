ce106eff0a83bfb5a5ab48b79d93588d0a278116
Ashlesha-Sonar <ashlesha@stockholmitacademy.org>
2024-02-13 14:58:46 +0100

    files updated

---
+++ session1/datatype.js
@@ -0,0 +1,11 @@
+let x=200;
+console.log(typeof(x));
+let stuname="Raj";
+console.log(typeof(stuname));
+let flag = true;
+console.log(typeof(flag));
+let a = null;
+console.log(typeof(a));
+let value;
+console.log(typeof(value));
+
---
+++ session1/operator.js
@@ -0,0 +1,40 @@
+let x =10,y=20;
+//Arathmatic Operators 
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(x**y);
+//Increment and decrement operator 
+x= x+1;
+console.log(x);
+x++;
+console.log(x);
+y=y-1;//
+console.log(y);
+y--;
+console.log(y);
+// Assingment Operators
+x=100,y=200
+console.log(x+=y);
+console.log(x-=y);
+console.log(x*y);
+console.log(x%=y);
+console.log(x/=y);
+//Assingement Oprators
+x =5,y=10;
+console.log(x>y);
+console.log(x<y);
+console.log(x>=y);
+console.log(x<=y);   
+console.log(x!=y);
+console.log(x==y);
+console.log(x<y?x:y);//?termanary Operator if condition is true print x otherwise print y
+console.log(y>x?x:y);
+//Logical Operators &&,||,!
+let a= true;
+let b= false;
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
